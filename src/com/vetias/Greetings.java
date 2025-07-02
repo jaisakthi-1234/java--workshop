f382311dd297393edfaba707a046ee62835704d4
jaisakthi-1234 <jaisakthiramasamy12@gmail.com>
2025-07-02 14:57:43 +0530

    mark code

--- src/com/vetias/Greetings.java
+++ src/com/vetias/Greetings.java
@@ -1,5 +1,4 @@
  package com.vetias;
-
 public class Greetings {
     public static void main(String[] args) {
         System.out.println("MY first java program");
---
+++ src/com/vetias/Mark.java
@@ -0,0 +1,12 @@
+package com.vetias;
+
+public class Mark {
+    public static void main(String[] args) {
+       int[] marks = {90, 80, 70, 60, 50};
+       for (int i = 0; i < marks.length; i++) {
+    System.out.println(marks[i]);
+}
+
+    }
+    
+}
