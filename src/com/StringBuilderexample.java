659d83cc8aa67f65d8e2519c118cf21174b83936
jaisakthi-1234 <jaisakthiramasamy12@gmail.com>
2025-07-07 14:26:24 +0530

    builder

---
+++ src/com/StringBuilderexample.java
@@ -0,0 +1,18 @@
+package com;
+
+public class StringBuilderexample {
+
+    public static void main(String[] args) {
+        StringBuilder sb= new StringBuilder();
+        sb.append("java");
+        sb.append( " is");
+        sb.append( " the");
+        sb.append(" most");
+        sb.append("  used");
+        sb.append(" language");
+        sb.append(" for");
+        sb.append(" enterprise");
+        sb.append(" application");
+        System.out.println(sb.toString());
+    }
+}
--- src/com/StringExample.java
+++ src/com/StringExample.java
@@ -8,13 +8,12 @@ public class StringExample {
         String fullname=firstname + " "+ lastname;
         System.out.println(fullname.toUpperCase());
 
-        String sentance = "this program shows how can we split into multiple string.thr long sentence eord and letter count";
-        String[] word = sentance.split("\\.");
-          System.out.println("Total sentence: " + sentance.length());
-        String[] words = sentance.split(" ");
-          System.out.println("Total chars" + words.length);
-          String[] chars = sentance.split(" ");
-          System.out.println("Total chars : "+ chars.length);
+        String sentance = "this program shows how can we split into multiple string .thelong sentence eord and letter count";
+        String[] words = sentance.split("");
+          System.out.println(words.length);
+        String dot[] = sentance.split("\\. ");
+        System.out.println(dot.length);
+        System.out.println(sentance.length());
         
       }
 
