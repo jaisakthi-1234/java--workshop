86e6ab28dbdb8d44cd109392f07220108cb1b088
jaisakthi-1234 <jaisakthiramasamy12@gmail.com>
2025-07-08 15:25:01 +0530

    add

---
+++ src/com/Calculatorexample.java
@@ -0,0 +1,31 @@
+package com;
+
+public class Calculatorexample {
+
+    public int divide(int a, int b) {
+
+        try {
+            if (b == 0) {
+                throw new ArithmeticException("cannot divisible by zero (0)");
+
+            }
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+        return a / b;
+    }
+    public int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) {
+
+        Calculatorexample calculator = new Calculatorexample();
+        System.out.println("Division: " + calculator.divide(6, 2));
+         System.out.println("Addition :"+ calculator.add(6, 3));
+        // System.out.println("Subtraction: " + calculator.subtract(10, 4));
+
+    }
+
+}
