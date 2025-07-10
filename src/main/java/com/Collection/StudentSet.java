18633c70b57fd1024adb5385c38535b1b113b9bd
jaisakthi-1234 <jaisakthiramasamy12@gmail.com>
2025-07-10 14:59:14 +0530

    roll

---
+++ src/main/java/com/Collection/StudentSet.java
@@ -0,0 +1,19 @@
+package com.Collection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class StudentSet {
+    public static void main(String[] args) {
+        Set<Integer> rollNos = new HashSet<>();
+       
+        rollNos.add(1001);
+        rollNos.add(1002);
+        rollNos.add(1001);
+        rollNos.add(1003);
+        rollNos.add(1004);
+        System.out.println(rollNos);
+        
+    }
+
+}
