08a5d5479baf037bd595f19f81272f7244afc84c
jaisakthi-1234 <jaisakthiramasamy12@gmail.com>
2025-07-15 09:45:15 +0530

    con

--- src/main/java/com/vetias/Temp/bean/dao/OrganationDao.java
+++ src/main/java/com/vetias/Temp/bean/dao/OrganationDao.java
@@ -6,31 +6,48 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class OrganationDao {
-    public void createTable() {
+   
+   public void createTable(Connection dbConnection) {
+       try(Statement statement= dbConnection.createStatement()){
+
+         statement.execute("""
+                     create table organation(
+                       NAME VARCHAR(100),
+                       WEBSITE VARCHAR(100),
+                       EMAIL VARCHAR(100),
+                       CONTACT_NUMBER VARCHAR(100),
+                       REGISTRATION_NO INT)
+                          """);
+     }catch(SQLException sqlException){
+          
+            System.out.println("error creating table:"+  sqlException);
+     }
+
+       }
    //   JdbcDataSource.h2DataSource =new JdbcDataScource();
    //   h2DataSource.setUr1("jdbc:h2:mem:tempdataDB");
    //   h2DataSource.setUser("sa");
-      try {
-         Class.forName("org.h2.Driver");
-      } catch (ClassNotFoundException e) {
-         // TODO Auto-generated catch block
-         e.printStackTrace();
-      }
+   //    try {
+   //       Class.forName("org.h2.Driver");
+   //    } catch (ClassNotFoundException e) {
+   //       // TODO Auto-generated catch block
+   //       e.printStackTrace();
+   //    }
 
-     try(Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:tempdataDB", "sa", null);
-        Statement statement = dbConnection.createStatement();){            
-                   statement.execute("""
-                      create table organation(
-                      id int AUTO_INCREMENT PRIMARY KEY,
-                      NAME VARCHAR(100),
-                      WEBSITE VARCHAR(100),
-                      EMAIL VARCHAR(100),
-                      CONTACT_NUMBER VARCHAR(100),
-                      REGISTRATION_NO INT)
-                      """);
-     }catch(SQLException e){
-        System.out.println("error creating table:"+ e.getMessage());
-     }
+   //   try(Connection dbConnection = DriverManager.getConnection("jdbc:h2:mem:tempdataDB", "sa", null);
+   //      Statement statement = dbConnection.createStatement();){            
+   //                 statement.execute("""
+   //                    create table organation(
+   //                    id int AUTO_INCREMENT PRIMARY KEY,
+   //                    NAME VARCHAR(100),
+   //                    WEBSITE VARCHAR(100),
+   //                    EMAIL VARCHAR(100),
+   //                    CONTACT_NUMBER VARCHAR(100),
+   //                    REGISTRATION_NO INT)
+   //                    """);
+   //   }catch(SQLException e){
+   //      System.out.println("error creating table:"+ e.getMessage());
+   //   }
 
 
 
